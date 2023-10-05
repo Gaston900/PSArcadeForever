22f7e72241fb93967212a3d816eb78b8ae7a60d7
unknownplayers940 <unknownplayers940@gmail.com>
2023-10-05 12:31:33 -0300

    Update

--- scripts/target/mame/mame.lua
+++ scripts/target/mame/mame.lua
@@ -10,11 +10,14 @@
 ---------------------------------------------------------------------------
 
 dofile("arcade.lua")
+dofile("mess.lua")
 
 function createProjects_mame_mame(_target, _subtarget)
 	createProjects_mame_arcade(_target, _subtarget)
+	createProjects_mame_mess(_target, _subtarget)
 end
 
 function linkProjects_mame_mame(_target, _subtarget)
 	linkProjects_mame_arcade(_target, _subtarget)
+	linkProjects_mame_mess(_target, _subtarget)
 end
