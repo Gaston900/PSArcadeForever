e991b8ca2f43b556d0e76766d1947f7612d47c43
LAPTOP-E6VUUOVQ\Gasto <unknownplayers940@gmail.com>
2024-10-17 15:49:41 -0300

    .

--- src/osd/winui/winui_opts.cpp
+++ src/osd/winui/winui_opts.cpp
@@ -59,7 +59,7 @@ const options_entry winui_options::s_option_entries[] =
 	{ MUIOPTION_HIDE_FOLDERS,				nullptr,        core_options::option_type::STRING, nullptr },
 	{ MUIOPTION_SHOW_FOLDER_SECTION,		"1",        core_options::option_type::BOOLEAN, nullptr },
 	{ MUIOPTION_SHOW_TABS,					"1",        core_options::option_type::BOOLEAN, nullptr },
-	{ MUIOPTION_HIDE_TABS,					"history",  core_options::option_type::STRING, nullptr },
+	{ MUIOPTION_HIDE_TABS,					"scores, howto, select, versus, boss, end, gameover, logo, artpreview, flyer, cabinet, marquee, cpanel, pcb, history",  core_options::option_type::STRING, nullptr },
 	{ MUIOPTION_HISTORY_TAB,				"18",       core_options::option_type::INTEGER, nullptr },
 	{ MUIOPTION_SORT_COLUMN,				"0",        core_options::option_type::INTEGER, nullptr },
 	{ MUIOPTION_SORT_REVERSED,				"0",        core_options::option_type::BOOLEAN, nullptr },
@@ -102,7 +102,7 @@ const options_entry winui_options::s_option_entries[] =
 
 	{ nullptr,								nullptr,       core_options::option_type::HEADER, "SEARCH PATH OPTIONS" },
 	{ MUIOPTION_ARTWORK_DIRECTORY,			"support/artpreview", core_options::option_type::STRING, nullptr },
-	{ MUIOPTION_AUDIO_DIRECTORY,			"config/audio",       core_options::option_type::STRING, nullptr },
+	{ MUIOPTION_AUDIO_DIRECTORY,			"audio",              core_options::option_type::STRING, nullptr },
 	{ MUIOPTION_BOSSES_DIRECTORY,			"support/bosses",     core_options::option_type::STRING, nullptr },
 	{ MUIOPTION_CABINET_DIRECTORY,			"support/cabinets",   core_options::option_type::STRING, nullptr },
 	{ MUIOPTION_CPANEL_DIRECTORY,			"support/cpanel",     core_options::option_type::STRING, nullptr },
